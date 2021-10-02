780e62f550702cf2e50844659be6c2e349d99903
Eddine Omar <eddine.farid@hotmail.fr>
2021-10-02 16:31:02 +0300

    add comment for future improvement

--- components/word/models.py
+++ components/word/models.py
@@ -5,6 +5,9 @@ from components.language.models import Language
 class Word(models.Model):
     name = models.CharField(max_length=150)
     language = models.ForeignKey(Language, on_delete=models.CASCADE)
+    # alpha_position = models.CharField(max_length=2, db_index=True, editable=True, blank=False, null=False, choices=ALPHABET_CHOICES, verbose_name=_('Alphabet Position'), help_text=_('Show this entry under which Alphabet position'))
+    # description = models.TextField(blank=False, verbose_name=_('The Explanation'), help_text=_('The description of the term being explained'))
+    # translation
 
     class Meta:
         ordering = ('name',)
