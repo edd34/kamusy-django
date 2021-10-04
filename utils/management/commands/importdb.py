08f37f0a08229b2e7cb4e7931568599138e6021d
Eddine Omar <eddine.farid@hotmail.fr>
2021-10-04 08:54:44 +0300

    update importdb command

--- utils/management/commands/importdb.py
+++ utils/management/commands/importdb.py
@@ -1,6 +1,5 @@
 from django.core.management.base import BaseCommand
-from django.utils import timezone
-from django.core.management.base import BaseCommand
+from django.core.management import call_command
 from components.word.models import Word
 from components.translation.models import Translation
 from components.language.models import Language
@@ -15,6 +14,17 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         df = main()
-        print(df)
-        print(Language, Word, Translation)
-        
+
+        mahorais = Language.objects.get_or_create(name="mahorais")
+        francais = Language.objects.get_or_create(name="français")
+
+        res = Language.objects.all()
+        print(res.values_list('pk', flat=True))
+
+        def create_translation(row):
+            word_mahorais = Word.objects.get_or_create(name=row["mahorais"], language=mahorais[0])
+            word_francais = Word.objects.get_or_create(name=row["français"], language=francais[0])
+            Translation.objects.get_or_create(word_source=word_mahorais[0], language_source=mahorais[0], word_destination=word_francais[0], language_destination=francais[0])
+            Translation.objects.get_or_create(word_source=word_francais[0], language_source=francais[0], word_destination=word_mahorais[0], language_destination=mahorais[0])
+
+        df.apply(lambda x: create_translation(x), axis=1)
