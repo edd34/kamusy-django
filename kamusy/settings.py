ef456c08f7d066961192d679d24907f0b696b98f
Eddine Omar <eddine.farid@hotmail.fr>
2021-10-12 19:45:29 +0300

    update settings and docker-compose prod

--- kamusy/settings.py
+++ kamusy/settings.py
@@ -20,13 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9(z-y=n)6t8g$4dlcal_jjh4tghm@#--5v3mwn3iu-x!kn67_z'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+if os.environ.get("DJANGO_ENV") == "dev":
+    SECRET_KEY = 'django-insecure-9(z-y=n)6t8g$4dlcal_jjh4tghm@#--5v3mwn3iu-x!kn67_z'
+    DEBUG = True
+    ALLOWED_HOSTS = []
+elif os.environ.get("DJANGO_ENV") == "prod":
+    SECRET_KEY = os.environ.get("SECRET")
+    DEBUG = False
+    ALLOWED_HOSTS = ["edd34.github.io"]
 
 
 # Application definition
